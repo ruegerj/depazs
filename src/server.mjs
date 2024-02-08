bced2530ac3d351be7ae0ce3c7ec343e89e33d7d
ruegerj <jan.rueger@gmx.ch>
2024-02-08 18:55:40 +0100

    split express app from server

--- src/server.mjs
+++ src/server.mjs
@@ -1,12 +1,29 @@
-import express from 'express';
+import app from './app.mjs';
 
-const app = express();
+// shutdown on uncaught exceptions
+process.on('uncaughtException', (err) => {
+    console.error(`[Fatal error] ${err.name}: ${err.message}`);
+    console.error('Shutting down app...');
 
-app.get('/', (req, res) => {
-    res.status(200).send('Hello Energy World :)');
+    process.exit(1);
 });
 
 const port = 3000;
 const server = app.listen(port, 'localhost', () => {
     console.log(`Listening on port ${port}...`);
+});
+
+// graceful shutdown on unhandled promise rejection
+process.on('unhandledRejection', (err) => {
+    console.error(`[Fatal error] ${err.name}: ${err.message}`);
+    console.error('Shutting down app...');
+
+    server.close(() => process.exit(1));
+});
+
+// graceful shutdown on termination
+process.on('SIGTERM', () => {
+    console.warn('[SIGTERM] Shutting down app...');
+
+    server.close();
 });
