93cd5cf3a4e54f104bb60a5df36d4e06b0848472
ruegerj <jan.rueger@gmx.ch>
2024-02-08 17:45:42 +0100

    setup prettier, editor cfg & recommended extensions

--- src/server.mjs
+++ src/server.mjs
@@ -1,12 +1,12 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello Energy World :)");
+app.get('/', (req, res) => {
+    res.status(200).send('Hello Energy World :)');
 });
 
 const port = 3000;
-const server = app.listen(port, "localhost", () => {
-  console.log(`Listening on port ${port}...`);
+const server = app.listen(port, 'localhost', () => {
+    console.log(`Listening on port ${port}...`);
 });
