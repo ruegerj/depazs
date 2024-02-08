a7be2ffc62230fce839a6e21ca5b6fa4af4cb949
ruegerj <jan.rueger@gmx.ch>
2024-02-08 17:33:16 +0100

    add basic express setup

---
+++ src/server.mjs
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello Energy World :)");
+});
+
+const port = 3000;
+const server = app.listen(port, "localhost", () => {
+  console.log(`Listening on port ${port}...`);
+});
