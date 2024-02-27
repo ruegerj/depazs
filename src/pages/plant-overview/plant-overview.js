481b23c2c277cfd40d0becef0c7d75f7b8bc19f5
ruegerj <jan.rueger@gmx.ch>
2024-02-27 23:32:45 +0100

    refactor js to prefer es6 definitions

--- src/pages/plant-overview/plant-overview.js
+++ src/pages/plant-overview/plant-overview.js
@@ -1,49 +1,59 @@
 function initializeMap() {
-    var mymap = L.map('map').setView([46.8182, 8.2275], 8);
+    const map = L.map('map').setView([46.8182, 8.2275], 8);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19
-    }).addTo(mymap);
+        maxZoom: 19,
+    }).addTo(map);
 
-    return mymap;
+    return map;
 }
 
 function addMarkerToMap(map, lat, lng, name, prices) {
-    var popupContent = "<div class='w3-container'><b>" + name + "</b><br>";
+    let popupContent = "<div class='w3-container'><b>" + name + '</b><br>';
 
-    var icons = {
-        'Electricity': 'fas fa-bolt',
-        'Gas': 'fas fa-fire',
-        'Oil': 'fas fa-oil-can'
+    const icons = {
+        Electricity: 'fas fa-bolt',
+        Gas: 'fas fa-fire',
+        Oil: 'fas fa-oil-can',
     };
 
     Object.keys(prices).forEach(function (energyType) {
         if (prices[energyType] !== null && prices[energyType] !== undefined) {
-            popupContent += "<p><i class='" + icons[energyType] + "'></i> " + energyType + " Price: <span class='w3-text-blue'>" + prices[energyType] + " CHF</span></p>";
+            popupContent +=
+                "<p><i class='" +
+                icons[energyType] +
+                "'></i> " +
+                energyType +
+                " Price: <span class='w3-text-blue'>" +
+                prices[energyType] +
+                ' CHF</span></p>';
         }
     });
 
-    popupContent += "</div>";
+    popupContent += '</div>';
 
-    var marker = L.marker([lat, lng]).addTo(map);
+    const marker = L.marker([lat, lng]).addTo(map);
     marker.bindPopup(popupContent);
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    var mymap = initializeMap();
+document.addEventListener('DOMContentLoaded', function () {
+    const map = initializeMap();
 
-    var mapElements = Array.from(document.getElementsByClassName("marker-data"));
+    const mapElements = Array.from(
+        document.getElementsByClassName('marker-data'),
+    );
 
     mapElements.forEach(function (element) {
-        var lat = element.getAttribute('data-lat');
-        var lng = element.getAttribute('data-lng');
-        var name = element.getAttribute('data-name');
-        var prices = {
-            'Electricity': element.getAttribute('data-electricity-price'),
-            'Gas': element.getAttribute('data-gas-price'),
-            'Oil': element.getAttribute('data-oil-price')
+        const lat = element.dataset.lat;
+        const lng = element.dataset.lng;
+        const name = element.dataset.name;
+
+        const prices = {
+            Electricity: element.dataset.electricityPrice,
+            Gas: element.dataset.gasPrice,
+            Oil: element.dataset.oilPrice,
         };
 
-        addMarkerToMap(mymap, lat, lng, name, prices);
+        addMarkerToMap(map, lat, lng, name, prices);
     });
 });
