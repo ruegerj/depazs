1dd18ead270034b30bcf79a153cbb9c49e20e6f1
ruegerj <jan.rueger@gmx.ch>
2024-02-27 23:45:42 +0100

    remove blocking methods from api endpoint

--- src/routes/api-routes.mjs
+++ src/routes/api-routes.mjs
@@ -1,49 +1,50 @@
 import { Router } from 'express';
 import path from 'node:path';
-import { readFileSync, writeFileSync } from 'node:fs';
+import { readFile, writeFile } from 'node:fs/promises';
 import { parseXml } from 'libxmljs2';
 
 const router = Router();
 const dbPath = path.resolve('database', 'energy-prices.xml');
 const xsdPath = path.resolve('database', 'energy-prices.schema.xsd');
 
-router.post('/addEnergyPrice', (req, res) => {
+router.post('/addEnergyPrice', async (req, res) => {
     const newEntry = req.body;
 
     // Get Plant
-    const database = getXmlDocument(dbPath);
+    const database = await getXmlDocument(dbPath);
 
     // Check if Price entry exists and has to be updated
     const targetPriceQuery = `//plant[@name="${newEntry.plantName}"]/prices/price[@type = "${newEntry.energyType}" and @date = "${newEntry.date}"]`;
     const possiblePrice = database.get(targetPriceQuery);
 
-    if(possiblePrice == null)
-    {
+    if (possiblePrice == null) {
         // Add node
         const targetPlantQuery = `//plant[@name="${newEntry.plantName}"]/prices`;
         const pricesOfPlant = database.get(targetPlantQuery);
-        pricesOfPlant.node('price', newEntry.price).attr("type", newEntry.energyType).attr('date', newEntry.date);
-    }
-    else
-    {
+        pricesOfPlant
+            .node('price', newEntry.price)
+            .attr('type', newEntry.energyType)
+            .attr('date', newEntry.date);
+    } else {
         // Update
-        possiblePrice.text(newEntry.price)
+        possiblePrice.text(newEntry.price);
     }
 
     //Validate
-    if (!database.validate(getXmlDocument(xsdPath))) {
-        res.status(400).send('Invalid XML, No tinkering here!')
-        return
+    const schema = await getXmlDocument(xsdPath);
+    if (!database.validate(schema)) {
+        res.status(400).send('Invalid XML, No tinkering here!');
+        return;
     }
 
-    writeFileSync(dbPath, database.toString(), 'utf-8')
+    await writeFile(dbPath, database.toString(), 'utf-8');
 
     res.send('OK');
 });
 
-function getXmlDocument(filePath)
-{
-    return parseXml(readFileSync(filePath, 'utf-8'));
+async function getXmlDocument(filePath) {
+    const xmlFile = await readFile(filePath, 'utf-8');
+    return parseXml(xmlFile);
 }
 
 export default router;
