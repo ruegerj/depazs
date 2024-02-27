678244294182118f27775367fe3b164e85fe9cdd
ruegerj <jan.rueger@gmx.ch>
2024-02-27 23:58:24 +0100

    tidy up app configuration

--- src/app.mjs
+++ src/app.mjs
@@ -6,22 +6,20 @@ import apiRoutes from './routes/api-routes.mjs';
 
 export function bootstrap() {
     const app = express();
-    app.use(express.urlencoded({ extended: true }));
 
-    // Add request logger
+    // add request logger
     if (process.env.NODE_ENV === 'development') {
         app.use(morgan('dev'));
     }
 
-    // Serve static files from the node_modules directory
+    // serve static content
     app.use('/node_modules', express.static(path.resolve('node_modules')));
-
-    // serve local db file & schema
     app.use('/database', express.static(path.resolve('database')));
-
-    // serve static file from /src/css directory
     app.use('/css', express.static(path.resolve('src', 'css')));
 
+    app.use(express.urlencoded({ extended: true }));
+
+    // configure page- & api-routes
     app.use(pageRoutes);
     app.use('/api', apiRoutes);
 
