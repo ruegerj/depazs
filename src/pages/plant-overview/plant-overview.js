c11ed60e5f3d737dbe4966f838bb08825926827c
Stocker Sven <sven.stocker@stud.hslu.ch>
2024-02-29 15:33:06 +0100

    refactor plant-overview.js

--- src/pages/plant-overview/plant-overview.js
+++ src/pages/plant-overview/plant-overview.js
@@ -1,53 +1,46 @@
 let cantonsLayer;
 
-function initializeMap() {
+async function initializeMap() {
     const map = L.map('map').setView([46.8182, 8.2275], 8);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
     }).addTo(map);
 
-    // load GeoJSON for swiss cantons
-    return fetch('/plant-overview/plant-overview.geojson')
-        .then(response => response.json())
-        .then(data => {
-            cantonsLayer = L.geoJson(data, {
-                style: function (feature) {
-                    return {
-                        weight: 1,
-                        opacity: 1,
-                        color: 'black',
-                    };
-                }
-            }).addTo(map);
-
-            return map;
-        });
+    // Load GeoJSON for Swiss cantons
+    try {
+        const response = await fetch('/plant-overview/plant-overview.geojson');
+        const data = await response.json();
+        cantonsLayer = L.geoJson(data, {
+            style: {
+                weight: 1,
+                opacity: 1,
+                color: 'black',
+            }
+        }).addTo(map);
+        return map;
+    } catch (error) {
+        console.error('Error initializing map:', error);
+    }
 }
 
 function addMarkerToMap(map, lat, lng, name, prices) {
-    let popupContent = "<div class='w3-container'><b>" + name + '</b><br>';
-
     const icons = {
         Electricity: 'fas fa-bolt',
         Gas: 'fas fa-fire',
         Oil: 'fas fa-oil-can',
     };
 
-    Object.keys(prices).forEach(function (energyType) {
-        if (prices[energyType] !== null && prices[energyType] !== undefined) {
-            popupContent +=
-                "<p><i class='" +
-                icons[energyType] +
-                "'></i> " +
-                energyType +
-                " Price: <span class='w3-text-blue'>" +
-                prices[energyType] +
-                ' CHF</span></p>';
-        }
-    });
+    const formattedPrices = Object.entries(prices)
+        .map(([energyType, price]) => {
+            if (price !== null && price !== undefined) {
+                return `<p><i class='${icons[energyType]}'></i> ${energyType} Price: <span class='w3-text-blue'>${price} CHF</span></p>`;
+            }
+            return '';
+        })
+        .join('');
 
-    popupContent += '</div>';
+    const popupContent = `<div class='w3-container'><b>${name}</b><br>${formattedPrices}</div>`;
 
     const marker = L.marker([lat, lng]).addTo(map);
     marker.bindPopup(popupContent);
@@ -56,36 +49,24 @@ function addMarkerToMap(map, lat, lng, name, prices) {
 function recolorCantons(cantons, color) {
     if (!cantonsLayer) return;
 
-    cantonsLayer.eachLayer(function (layer) {
+    cantonsLayer.eachLayer(layer => {
         if (cantons.includes(layer.feature.properties.KUERZEL)) {
-            layer.setStyle({ fillColor: color, fillOpacity: 0.7});
+            layer.setStyle({ fillColor: color, fillOpacity: 0.5 });
         }
     });
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    initializeMap().then(map => {
-        const mapElements = Array.from(
-            document.getElementsByClassName('marker-data'),
-        );
+document.addEventListener('DOMContentLoaded', async () => {
+    const map = await initializeMap();
+    const mapElements = Array.from(document.getElementsByClassName('plant-data'));
+    const colors = ['green', 'blue', 'red', 'yellow', 'orange', 'purple', 'pink', 'brown', 'grey', 'cyan', 'magenta', 'lime'];
 
-        const colors = ['green', 'blue', 'red', 'yellow', 'orange', 'purple', 'pink', 'brown', 'grey', 'cyan', 'magenta', 'lime'];
+    mapElements.forEach((element, index) => {
+        const { lat, lng, name, electricityPrice, gasPrice, oilPrice, cantons } = element.dataset;
+        const prices = { Electricity: electricityPrice, Gas: gasPrice, Oil: oilPrice };
+        const cantonList = cantons.split('\n').map(canton => canton.trim()).filter(Boolean);
 
-        mapElements.forEach(function (element, index) {
-            const lat = element.dataset.lat;
-            const lng = element.dataset.lng;
-            const name = element.dataset.name;
-
-            const prices = {
-                Electricity: element.dataset.electricityPrice,
-                Gas: element.dataset.gasPrice,
-                Oil: element.dataset.oilPrice,
-            };
-
-            addMarkerToMap(map, lat, lng, name, prices);
-
-            const cantons = element.dataset.cantons.split('\n').map(canton => canton.trim()).filter(Boolean);
-            recolorCantons(cantons, colors[index % colors.length]);
-        });
+        addMarkerToMap(map, lat, lng, name, prices);
+        recolorCantons(cantonList, colors[index % colors.length]);
     });
 });
